bb8667684410c5687e484f2372323703e8a45696
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2021-03-10 12:42:57 +0700

    React Basics Tutorial---4. JSX Rules

--- react-basics-tutorial/src/index.js
+++ react-basics-tutorial/src/index.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDom from "react-dom";
+
+// JSX Rules
+// return single Element
+// div/section/article or Fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
 
 function Greeting() {
   return (
-    <div>
+    <React.Fragment>
       <h1>Hello World</h1>
-    </div>
+
+      <ul>
+        <li>
+          <a href="#">Li 1</a>
+        </li>
+
+        <li>
+          <a href="#">Li 2</a>
+        </li>
+
+        <li>
+          <a href="#">Li 3</a>
+        </li>
+      </ul>
+
+      <div></div>
+    </React.Fragment>
   );
 }
 
