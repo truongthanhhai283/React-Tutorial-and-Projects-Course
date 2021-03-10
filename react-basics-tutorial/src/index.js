1942c2438320f80a598820acb5d46dafa038f5c2
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2021-03-10 20:53:45 +0700

    React Basics Tutorial---10. Props

--- react-basics-tutorial/src/index.js
+++ react-basics-tutorial/src/index.js
@@ -1,37 +1,68 @@
 import React from "react";
 import ReactDom from "react-dom";
-import './index.css';
+import "./index.css";
 
 //Js
+
+//setup vars
+const bookVar1 = {
+  imgLink:
+    "https://static01.nyt.com/images/2019/11/21/books/best-books-2019/best-books-2019-mobileMasterAt3x.jpg",
+  title: "Title book",
+  AuthorBook: "Hai",
+};
+
+
+const bookVar2 = {
+  imgLink:
+    "https://static01.nyt.com/images/2019/11/21/books/best-books-2019/best-books-2019-mobileMasterAt3x.jpg",
+  title: "Title book",
+  AuthorBook: "Hai",
+};
+
+const bookVar3 = {
+  imgLink:
+    "https://static01.nyt.com/images/2019/11/21/books/best-books-2019/best-books-2019-mobileMasterAt3x.jpg",
+  title: "Title book",
+  AuthorBook: "Hai",
+};
+
+const bookVar4 = {
+  imgLink:
+    "https://static01.nyt.com/images/2019/11/21/books/best-books-2019/best-books-2019-mobileMasterAt3x.jpg",
+  title: "Title book",
+  AuthorBook: "Hai",
+};
+
+
+// const title = "Title book";
+// const AuthorBook = "Hai";
+// const linkImage =
+//   "https://static01.nyt.com/images/2019/11/21/books/best-books-2019/best-books-2019-mobileMasterAt3x.jpg";
 
 function BookList() {
   return (
     <section className="content">
-      <Book />
+      <Book job="dev" img={bookVar1.imgLink} title={bookVar1.title} author={bookVar1.AuthorBook}/>
+      <Book job="dev" img={bookVar2.imgLink} title={bookVar2.title} author={bookVar2.AuthorBook}/>
+      <Book job="dev" img={bookVar3.imgLink} title={bookVar3.title} author={bookVar3.AuthorBook}/>
+      <Book job="dev" img={bookVar4.imgLink} title={bookVar4.title} author={bookVar4.AuthorBook}/>
     </section>
   );
 }
 
-const author="Truong Thanh Hai"
-
-const Book = () => {
-  const title="Title book"
+const Book = (props) => {
+  console.log(props);
   return (
     <article>
-      <h1>{title}</h1>
-      <h1>{author.toUpperCase()}</h1>
-      <img
-      src="https://static01.nyt.com/images/2019/11/21/books/best-books-2019/best-books-2019-mobileMasterAt3x.jpg"
-      alt=""
-    />
-    <p>{6+12}</p>
+      <h1>{props.title}</h1>
+      <h1>{props.author}</h1>
+      <img src={props.img} alt="" />
+      {/* <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p> */}
     </article>
   );
 };
-
-
-const Author = () => <h3 style={{color:'blue',fontSize:'20px'}}>Hai</h3>
-
-
 
 ReactDom.render(<BookList />, document.getElementById("root"));
