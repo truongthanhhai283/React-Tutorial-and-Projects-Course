31adf0f9164db6aff1394a9a605bd3d5ac049ae9
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2021-03-11 13:42:01 +0700

    React Hooks and Advanced Topic---9. useState - Object Example

--- react-hooks-and-advanced-topics/src/App.js
+++ react-hooks-and-advanced-topics/src/App.js
@@ -1,8 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Setup from './tutorial/setup/3-useState-array';
+import Setup from './tutorial/setup/4-useState-object';
 
-import Final from "./tutorial/final/3-useState-array";
+import Final from "./tutorial/final/4-useState-object";
 
 function App() {
   return (
