952b83feaa72e2829b85e413df5e35e19932f6a2
HaiTruong <58763134+truongthanhhai283@users.noreply.github.com>
2021-03-11 09:53:56 +0700

    React Hooks and Advanced Topics--5. useState - Basics

--- react-hooks-and-advanced-topics/src/App.js
+++ react-hooks-and-advanced-topics/src/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Setup from "./tutorial/setup/1-error-example";
+import Setup from "./tutorial/setup/2-useState-basics";
+
+import Final from "./tutorial/final/2-useState-basics";
 
 function App() {
   return (
     <div className="container">
       <Setup />
+      <Final />
     </div>
   );
 }
