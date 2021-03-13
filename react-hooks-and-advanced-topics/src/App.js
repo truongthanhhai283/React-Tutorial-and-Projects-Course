8fea1fb75f9575e83e16ca0c7954a43d45953080
TruongThanhHai28 <truongthanhhai283@gmail.com>
2021-03-13 17:18:58 +0700

    React Hooks and Advanced Topic---20. Multiple Returns - Fetching Example

--- react-hooks-and-advanced-topics/src/App.js
+++ react-hooks-and-advanced-topics/src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
+// import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
 
+import Final from "./tutorial/3-conditional-rendering/final/1-multiple-returns";
 
 function App() {
   return (
     <div className="container">
-      <Setup />
+      {/* <Setup /> */}
+      <Final />
     </div>
   );
 }
