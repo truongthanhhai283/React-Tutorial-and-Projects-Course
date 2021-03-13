5453d2bb8835c9516062c14017d7aabe5ba83511
TruongThanhHai28 <truongthanhhai283@gmail.com>
2021-03-13 17:16:31 +0700

    React Hooks and Advanced Topic---19. Multiple Returns - Basics

--- react-hooks-and-advanced-topics/src/App.js
+++ react-hooks-and-advanced-topics/src/App.js
@@ -1,14 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Setup from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
+import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
 
-import Final from "./tutorial/2-useEffect/final/3-useEffect-fetch-data";
 
 function App() {
   return (
     <div className="container">
       <Setup />
-      <Final />
     </div>
   );
 }
