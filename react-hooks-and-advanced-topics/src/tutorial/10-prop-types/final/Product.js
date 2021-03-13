9236c581cd04e6e2e75b549e64a067afc338d345
TruongThanhHai28 <truongthanhhai283@gmail.com>
2021-03-13 20:04:38 +0700

    React Hooks and Advanced Topic---42. PropTypes - Default Values

--- react-hooks-and-advanced-topics/src/tutorial/10-prop-types/final/Product.js
+++ react-hooks-and-advanced-topics/src/tutorial/10-prop-types/final/Product.js
@@ -4,11 +4,14 @@ import defaultImage from '../../../assets/default-image.jpeg';
 
 const Product = ({ image, name, price }) => {
   const url = image && image.url;
+  // const url = image && image.url;
   return (
     <article className='product'>
       <img src={url || defaultImage} alt={name || 'default name'} />
+      {/* <img src={url} alt={name} /> */}
       <h4>{name}</h4>
       <p>${price || 3.99}</p>
+      {/* <p>${price}</p> */}
     </article>
   );
 };
