b1d331d00d75e17a846cf151614188b157268e4b
TruongThanhHai28 <truongthanhhai283@gmail.com>
2021-03-16 14:55:15 +0700

    9. Footer

--- e-commerce-project/src/components/Footer.js
+++ e-commerce-project/src/components/Footer.js
@@ -1,8 +1,14 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 const Footer = () => {
-  return <h4>footer</h4>
-}
+  return (
+    <Wrapper>
+      <h5>
+        COPYRIGHT © {new Date().getFullYear()} <span>Truong Thanh Hai</span>
+      </h5>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.footer`
   height: 5rem;
@@ -23,9 +29,13 @@ const Wrapper = styled.footer`
     text-transform: none;
     line-height: 1.25;
   }
+
+  span {
+    color: #38b7ee;
+  }
   @media (min-width: 776px) {
     flex-direction: row;
   }
-`
+`;
 
-export default Footer
+export default Footer;
